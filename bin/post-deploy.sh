936e9608b84fdbd42587cf85a47cdac1b048b695
Anthony Ettinger <anthony@chovy.com>
2018-11-11 12:41:18 -0800

    add nginx configurations

--- bin/post-deploy.sh
+++ bin/post-deploy.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
-# load node v6
+# load node
 . $HOME/.bashrc
-. "$NVM_DIR/nvm.sh" && nvm use v12
+. "$NVM_DIR/nvm.sh" && nvm use v11
 
-production=$YLLA_PRODUCTION_HOST
 host=$(hostname)
 name=ylla
 project=ylla-web
 
+. $HOME/www/${name}/${project}/.env.sh
+
+production=$YLLA_PRODUCTION_HOST
+development=$YLLA_DEVELOPMENT_HOST
+
 node -v
 npm -v
 
-echo "current host: $host production: $production"
+echo "current host: $host production: $production development: $development"
 
-if [[ $host == "$production" ]]; then
+if [[ $host == "$production" ]] || [[ $host == "$development" ]]; then
   cd $HOME/www/${name}/${project}
-  #npm i
-  #npm run start:cluster &
+  # npm run restart:production
 fi
